e5c455954e05a1102ef627dd05a77c748ab5277e
hakanolgun <kaptanhakanolgun@gmail.com>
2021-08-20 12:57:28 +0300

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,219 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { name: "Learn Javascript", isCompleted: false },
+    { name: "Learn React", isCompleted: false },
+  ]);
+  const [inputvalue, setInputValue] = useState("");
+  const tick = document.getElementsByClassName("toggle");
+  const liler = document.getElementsByClassName("myli");
+  const filterbtns = document.getElementsByClassName("filterbtn");
+
+  const handleActive = (item) => {
+    setTodoList(
+      todoList.map((newitem) =>
+        newitem === item
+          ? { ...newitem, isCompleted: !newitem.isCompleted }
+          : newitem
+      )
+    );
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodoList([
+      ...todoList,
+      {
+        isCompleted: false,
+        name: inputvalue,
+      },
+    ]);
+
+    setInputValue("");
+  };
+
+  const deleteItem = (item) => {
+    setTodoList(todoList.filter((items) => items !== item));
+  };
+
+  const handleAllActive = () => {
+    const hepsiCompletedMi = todoList.every((item) => item.isCompleted);
+    const hepsiFalseMu = todoList.every((item) => !item.isCompleted);
+
+    if (hepsiCompletedMi) {
+      setTodoList(
+        todoList.map((item) =>
+          item
+            ? { ...item, isCompleted: false }
+            : { ...item, isCompleted: true }
+        )
+      );
+      for (let i = 0; i < tick.length; i++) {
+        tick[i].checked = false;
+      }
+    } else if (hepsiFalseMu) {
+      setTodoList(
+        todoList.map((item) =>
+          item
+            ? { ...item, isCompleted: true }
+            : { ...item, isCompleted: false }
+        )
+      );
+      for (let i = 0; i < tick.length; i++) {
+        tick[i].checked = true;
+      }
+    } else {
+      setTodoList(
+        todoList.map((item) =>
+          item
+            ? { ...item, isCompleted: true }
+            : { ...item, isCompleted: false }
+        )
+      );
+      for (let i = 0; i < tick.length; i++) {
+        if (tick[i].checked === false) {
+          tick[i].checked = true;
+        }
+      }
+    }
+  };
+
+  const handleFilterAll = (e) => {
+    //tıklanan butona css veriyorum, seçili olan görülsün diye
+    for (let z = 0; z < filterbtns.length; z++) {
+      filterbtns[z].classList.remove("selected");
+    }
+    e.target.classList.add("selected");
+
+    //class'ı gizli olan tüm li elementlerini tekrar görünür yapıyorum
+    for (let i = 0; i < liler.length; i++) {
+      liler[i].classList.remove("hidden");
+    }
+  };
+  const handleFilterActive = (e) => {
+    for (let z = 0; z < filterbtns.length; z++) {
+      filterbtns[z].classList.remove("selected");
+    }
+    e.target.classList.add("selected");
+
+    //completed olan lileri gizliyorum, diğerlerini görünür yapıyorum
+    for (let i = 0; i < liler.length; i++) {
+      if (liler[i].classList.contains("completed")) {
+        liler[i].classList.add("hidden");
+      } else {
+        liler[i].classList.remove("hidden");
+      }
+    }
+  };
+  const handleFilterCompleted = (e) => {
+    for (let z = 0; z < filterbtns.length; z++) {
+      filterbtns[z].classList.remove("selected");
+    }
+    e.target.classList.add("selected");
+
+    //completed olan lileri görünür yapıp, diğerlerini gizliyorum
+    for (let i = 0; i < liler.length; i++) {
+      if (!liler[i].classList.contains("completed")) {
+        liler[i].classList.add("hidden");
+      } else {
+        liler[i].classList.remove("hidden");
+      }
+    }
+  };
+
+  const handleClearCompleted = () => {
+    setTodoList(todoList.filter((item) => !item.isCompleted));
+  };
+
+  useEffect(() => {
+    filterbtns[0].click();
+  }, [filterbtns]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input
+            className="toggle-all"
+            title="Mark all as completed"
+            type="checkbox"
+            onClick={handleAllActive}
+          />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="new-todo"
+              placeholder="What needs to be done?"
+              value={inputvalue}
+              autofocus
+              onChange={(e) => handleInputChange(e)}
+            />
+          </form>
+        </header>
+
+        <section className="main">
+          <ul className="todo-list">
+            {todoList.map((item, key) => (
+              <li
+                className={item.isCompleted ? "completed myli" : "myli"}
+                key={key}
+              >
+                <div className="view">
+                  <input
+                    className="toggle"
+                    type="checkbox"
+                    onClick={() => handleActive(item)}
+                  />
+                  <label>{item.name}</label>
+                  <button
+                    className="destroy"
+                    onClick={() => deleteItem(item)}
+                  ></button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>{todoList.length}</strong>
+            &#x200B; items left
+          </span>
+
+          <div className="filters">
+            <button
+              onClick={(e) => handleFilterAll(e)}
+              className="filterbtn allbtn"
+            >
+              All
+            </button>
+
+            <button
+              onClick={(e) => handleFilterActive(e)}
+              className="filterbtn"
+            >
+              Active
+            </button>
+
+            <button
+              onClick={(e) => handleFilterCompleted(e)}
+              className="filterbtn"
+            >
+              Completed
+            </button>
+          </div>
+
+          <button className="clear-completed" onClick={handleClearCompleted}>
+            Clear completed
+          </button>
+        </footer>
+      </section>
     </div>
   );
 }
